5a5cacf5c28eaf6269c9e0ce7dcb877a2106a0c4
dongjianpeng <dongjianpeng@gmail.com>
2018-09-26 00:38:41 +0800

    life update

--- app/src/main/java/top/murphypen/studyandroid/activity/View02/ActivityLife.java
+++ app/src/main/java/top/murphypen/studyandroid/activity/View02/ActivityLife.java
@@ -1,9 +1,12 @@
 package top.murphypen.studyandroid.activity.View02;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import top.murphypen.studyandroid.activity.R;
 import top.murphypen.studyandroid.activity.base.BaseActivity;
+import top.murphypen.studyandroid.activity.base.value.OpenViewValue;
+import top.murphypen.studyandroid.util.ToastUtil;
 
 public class ActivityLife extends BaseActivity {
 
@@ -12,7 +15,26 @@ public class ActivityLife extends BaseActivity {
         showLifeLogger();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life);
+
+        if (savedInstanceState != null) {
+            String value = savedInstanceState.getString("save");
+            ToastUtil.show(this, value);
+        }
+
+//        addOnClickListener(R.id.start_normal_btn,R.id.start_dialog_btn);
+        addOnClickListenerForOpenView(new OpenViewValue(R.id.start_normal_btn, NormalActivity.class),
+                new OpenViewValue(R.id.start_dialog_btn, DialogActivity.class));
     }
 
 
+    /**
+     * 在活动被系统回收时一定会调用的方法，在Bundle中保存数据，重新进入活动时用savedInstanceState恢复数据
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("save", "Value....");
+    }
 }
